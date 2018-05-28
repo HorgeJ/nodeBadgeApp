73158366d180f627a218f075af7b0d53705d3449
Jorge Sagastume <justhorge@gmail.com>
2018-05-27 17:06:53 -0700

    Add function to print message to console and test

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // App that connects to treehouse API and displays users badges using the command line argument.
 
+
+// Function to print message to console.
+function printMessage(username,badgeCount,points){
+    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`; // var to hold message
+    console.log(message);   // print message to console
+}
+
+printMessage("Horge", 100, 328);
+
 // Connect to API URL
 // Read the data
 // Parse the data
