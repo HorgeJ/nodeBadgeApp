024130b4d7ca6e5e98aa979cff464bfc74abb9a0
Jorge Sagastume <justhorge@gmail.com>
2018-05-28 23:56:37 -0700

    connect to API url

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 // App that connects to treehouse API and displays users badges using the command line argument.
-
-
+const username = "justhorge";
+const https = require('https'); // require https module
 // Function to print message to console.
 function printMessage(username,badgeCount,points){
     const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`; // var to hold message
     console.log(message);   // print message to console
 }
 
-printMessage("Horge", 100, 328);
-
 // Connect to API URL
-// Read the data
-// Parse the data
-// Print the data
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+    // Read the data
+    console.log(response.statusCode);
+    // Parse the data
+    // Print the data
+});
